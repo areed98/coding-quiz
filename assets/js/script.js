e3ff7a969b6e4059a88f1624dd2d4c6e07c7430c
areed98 <austinjreed89@gmail.com>
2022-10-13 17:42:11 -0400

    JavaScript almost complete, stylesheet mostly updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -204,6 +204,47 @@ var questionConstructor = function(question) {
 
 };
 
+// check for final question
+var finalQuestion = function(event) {
+
+    // Created after the questionChecker, this is to make sure there are no errors
+    // and that the quiz will stop after the final question.
+    var correctAns = questionArray[questionID].correctAns;
+    match = event.match;
+
+    // Set the finalquestionID to the questions array length minus 1 so that we execute logic on the final question,
+    // rather than AFTER the final question.
+    finalQuestionID = questionArray.length - 1;
+
+    if (questionID === finalQuestionID) {
+
+        clearInterval(timerInterval.interval);
+
+        if (match.classList.contains("answer")) {
+
+            var correctAnswer = document.getElementById(correctAns);
+
+            if (match === correctAnswer) {
+
+                gameOver();
+            } else {
+                if (counter > 10) {
+                    counter = counter - 10;
+                    time.textContent = ("Time: " + counter);
+                    gameOver();
+                } else {
+                    counter = 0; 
+                    time.textContent = "Time: 0";
+                    gameOver();
+                }
+            }
+        }
+    }
+
+
+};
+
+
 // Create logic for if the correct answer is chosen.
 var questionChecker = function(match) {
 
@@ -285,6 +326,212 @@ var questionChecker = function(match) {
     }
 };
 
+
+// Logic to send user to end screen
 var gameOver = function() {
-    
-}
+
+    // Element generation
+    var sectionEl = document.createElement("section");
+    var h2El = document.createElement("h2");
+    var h3El = document.createElement("h3");
+    var formEl = document.createElement("form");
+    var labelEl = document.createElement("label");
+    var inputEl = document.createElement("input");
+    var submitEl = document.createElement("input");
+
+    // Set Attributes to elements
+    sectionEl.setAttribute("id", "gameOverScreen");
+    h2El.setAttribute("id", "gameOverScreenTitle");
+    h3El.setAttribute("id", "gameOverScreenSub");
+    labelEl.setAttribute("for", "intitials");
+    inputEl.setAttribute("id", "intials");
+    submitEl.setAttribute("type", "submit");
+    submitEl.setAttribute("value", "submit");
+    submitEl.setAttribute("id", "submit");
+
+    // Set text content
+    h2El.textContent = "All done!";
+    h3El.textContent = "Your final score is " + counter + "!";
+    labelEl.textContent = "Enter Initials!";
+
+    // Append elements
+    formEl.appendChild(labelEl);
+    formEl.appendChild(inputEl);
+    formEl.appendChild(submitEl);
+    sectionEl.appendChild(h2El);
+    sectionEl.appendChild(h3El);
+    sectionEl.appendChild(formEl);
+    flexEl.appendChild(sectionEl);
+
+    // Remove bottom correct/incorrect border and p element
+    flexEl.style.borderBottom = "none";
+    pEl.remove();
+
+    // Submit button listener
+    formEl.addEventListener("Submit", function(event) {
+
+        var input = document.getElementById("initials").value
+
+        // Validate input
+        if (input === null || input === "" || input.length > 3) {
+
+            alert("Please input initials.");
+
+            formEl.reset();
+
+        // If user inputs proper initials, push values into an object and then into a score array
+        } else {
+            var score = counter;
+
+            var userValues = {
+                userName: input,
+                userScore: score
+            }
+
+            scores.push(userValues);
+            // saves scores
+            saveScores()
+            // resets inputs
+            formEl.reset();
+            // Go to high score page
+            highScorePage();
+        };
+
+
+    });
+};
+
+// Logic for high score page
+var highScorePage = function() {
+
+    // remove both timer and high scores page link
+    if (document.getElementById("time")) {
+        var timeEl = document.getElementById("time");
+        timeEl.textContent = "";
+    }
+
+    if (document.querySelector(".hscore")) {
+        var viewHS = document.querySelector(".hscore");
+        viewHS.textContent = "";
+        viewHS.removeEventListener("click", highScorePage);
+        viewHS.addEventListener("click", homePage);
+    }
+
+    // If on game over screen, remove element first
+    if (document.getElementById("gameOverScreen")) {
+        var OverEl = document.getElementById("gameOverScreen");
+        OverEl.remove();
+    }
+
+    if (document.getElementById("begin")) {
+        var beginEl = document.getElementById("begin");
+        beginEl.remove();
+    }
+
+    // Create elements
+    var sectionEl = document.createElement("section");
+    var h2El = document.createElement("h2");
+    var divEl1 = document.createElement("div");
+    var divEl2 = document.createElement("div");
+    var button1 = document.createElement("button");
+    var button2 = document.createElement("button");
+
+    // Run a for loop to create an element for each high score
+    for (var i = 0; i < scores.length; i++) {
+
+        // Create element
+        var scoreEl = document.createElement("p");
+
+        // Set text
+        scoreEl.textContent = (i + 1) + "." + scores[i].userName + " - " + scores[i].userScore;
+
+        // give class of score
+        scoreEl.setAttribute("class", "score");
+
+        // append
+        divEl1.appendChild(scoreEl);
+    }
+
+
+    // Attribute set
+    sectionEl.setAttribute("id", "scoreboard");
+    h2El.setAttribute("id", "scoreboardTitle");
+    divEl1.setAttribute("class", "scores");
+    divEl2.setAttribute("class", "scoreDiv2");
+    button1.setAttribute("class", "scoreButton");
+    button2.setAttribute("class", "scoreButton");
+
+    // set text content
+    h2El.textContent = "High Scores:";
+    button1.textContent = "Go back";
+    button2.textContent = "Clear High Scores";
+
+    // Append
+    divEl1.appendChild(button1);
+    divEl2.appendChild(button2);
+    sectionEl.appendChild(h2El);
+    sectionEl.appendChild(divEl1);
+    sectionEl.appendChild(divEl2);
+    flexEl.appendChild(sectionEl);
+
+    // Add event listeners for buttons
+    button1.addEventListener("click", homePage);
+    button2.addEventListener("click", clearScores);
+
+};
+
+// Logic to save scores
+var saveScores = function() {
+
+    sortScores();
+
+    localStorage.setItem("scores", JSON.stringify(scores));
+
+};
+
+// Logic to load scores
+var loadScores = function() {
+
+    //use local storage to find "scores"
+    var savedScores = localStorage.getItem("scores");
+
+    // if empty, return
+    if (!savedScores) {
+        return false;
+    }
+
+    // parse contents back into array
+    savedScores = JSON.parse(savedScores);
+
+    for (var i = 0;  i < savedScores.length; i++) {
+        scores.push(savedScores[i]);
+    }
+
+    sortScores();
+
+};
+
+// function to sort the scores
+var sortScores = function() {
+
+    scores.sort((a, b) => b.userScore - a.userScore);
+};
+
+// logic to clear scores
+var clearScores = function() {
+
+    scores = [];
+
+    localStorage.setItem("scores", scores);
+
+    if (document.getElementById("scoreboard")) {
+        var scoreboard = document.getElementById("scoreboard");
+        scoreboard.remove();
+    }
+
+    highScorePage();
+};
+
+//load scores from local storage
+loadScores();
+
